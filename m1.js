9b063e67e0628c597f73d5db02f3dd60decefd54
RTJ <46841637+RTJ99@users.noreply.github.com>
2024-01-15 01:41:22 +0200

    Update m1.js

--- m1.js
+++ m1.js
@@ -179,7 +179,6 @@ function addMapPoints() {
   map.addLayer({
     id: "locations",
     type: "circle",
-
     source: {
       type: "geojson",
       data: mapLocations,
@@ -190,6 +189,58 @@ function addMapPoints() {
       "circle-color": "#AA000D",
       "circle-opacity": 1,
       "circle-stroke-color": "#ffffff",
+    },
+  });
+
+  map.addSource("locations", {
+    type: "geojson",
+    data: mapLocations,
+    cluster: true,
+    clusterMaxZoom: 14,
+    clusterRadius: 50,
+  });
+
+  map.addLayer({
+    id: "clusters",
+    type: "circle",
+    source: "locations",
+    filter: ["has", "point_count"],
+    paint: {
+      "circle-color": [
+        "step",
+        ["get", "point_count"],
+        "#51bbd6",
+        100,
+        "#f1f075",
+        750,
+        "#f28cb1",
+      ],
+      "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+    },
+  });
+
+  map.addLayer({
+    id: "cluster-count",
+    type: "symbol",
+    source: "locations",
+    filter: ["has", "point_count"],
+    layout: {
+      "text-field": ["get", "point_count_abbreviated"],
+      "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+      "text-size": 12,
+    },
+  });
+
+  map.addLayer({
+    id: "unclustered-point",
+    type: "circle",
+    source: "locations",
+    filter: ["!", ["has", "point_count"]],
+    paint: {
+      "circle-color": "#11b4da",
+      "circle-radius": 4,
+      "circle-stroke-width": 1,
+      "circle-stroke-color": "#fff",
     },
   });
 
@@ -291,66 +342,7 @@ map.on("load", function (e) {
 
   mapLocations.features.push(defaultFeature);
 
-    addMapPoints();
-     map.addSource("locations", {
-       type: "geojson",
-       data: mapLocations,
-       cluster: true,
-       clusterMaxZoom: 14,
-       clusterRadius: 50,
-     });
-
-     map.addLayer({
-       id: "clusters",
-       type: "circle",
-       source: "locations",
-       filter: ["has", "point_count"],
-       paint: {
-         "circle-color": [
-           "step",
-           ["get", "point_count"],
-           "#51bbd6",
-           100,
-           "#f1f075",
-           750,
-           "#f28cb1",
-         ],
-         "circle-radius": [
-           "step",
-           ["get", "point_count"],
-           20,
-           100,
-           30,
-           750,
-           40,
-         ],
-       },
-     });
-
-     map.addLayer({
-       id: "cluster-count",
-       type: "symbol",
-       source: "locations",
-       filter: ["has", "point_count"],
-       layout: {
-         "text-field": ["get", "point_count_abbreviated"],
-         "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-         "text-size": 12,
-       },
-     });
-
-     map.addLayer({
-       id: "unclustered-point",
-       type: "circle",
-       source: "locations",
-       filter: ["!", ["has", "point_count"]],
-       paint: {
-         "circle-color": "#11b4da",
-         "circle-radius": 4,
-         "circle-stroke-width": 1,
-         "circle-stroke-color": "#fff",
-       },
-     });
+  addMapPoints();
 });
 
 $(".close-block").click(function () {
