84177645727c3f4b9de204295727bb6dbbbccfe0
RTJ <46841637+RTJ99@users.noreply.github.com>
2024-01-17 00:23:11 +0200

    Update and rename mapp28.js to mapp29.js

--- mapp29.js
+++ mapp29.js
@@ -226,21 +226,75 @@ function searchByName() {
 }
 
 function addMapPoints() {
-  map.addLayer({
-    id: "locations",
-    type: "circle",
-
+    map.addLayer({
+    id: 'locations',
+    type: 'circle',
     source: {
-      type: "geojson",
+      type: 'geojson',
       data: mapLocations,
+      cluster: true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50
     },
     paint: {
-      "circle-radius": 8,
-      "circle-stroke-width": 1,
-      "circle-color": "#AA000D",
-      "circle-opacity": 1,
-      "circle-stroke-color": "#ffffff",
-    },
+      'circle-radius': 8,
+      'circle-stroke-width': 1,
+      'circle-color': '#AA000D',
+      'circle-opacity': 1,
+      'circle-stroke-color': '#ffffff'
+    }
+  });
+
+  map.addLayer({
+    id: 'clusters',
+    type: 'circle',
+    source: 'locations',
+    filter: ['has', 'point_count'],
+    paint: {
+      'circle-color': [
+        'step',
+        ['get', 'point_count'],
+        '#51bbd6',
+        100,
+        '#f1f075',
+        750,
+        '#f28cb1'
+      ],
+      'circle-radius': [
+        'step',
+        ['get', 'point_count'],
+        20,
+        100,
+        30,
+        750,
+        40
+      ]
+    }
+  });
+
+  map.addLayer({
+    id: 'cluster-count',
+    type: 'symbol',
+    source: 'locations',
+    filter: ['has', 'point_count'],
+    layout: {
+      'text-field': ['get', 'point_count_abbreviated'],
+      'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+      'text-size': 12
+    }
+  });
+
+  map.addLayer({
+    id: 'unclustered-point',
+    type: 'circle',
+    source: 'locations',
+    filter: ['!', ['has', 'point_count']],
+    paint: {
+      'circle-color': '#11b4da',
+      'circle-radius': 4,
+      'circle-stroke-width': 1,
+      'circle-stroke-color': '#fff'
+    }
   });
 
   function addPopup(e) {
