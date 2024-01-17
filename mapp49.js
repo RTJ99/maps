fee5d14bba8fc9a1ad7dcb9514e8b44d346e1421
RTJ <46841637+RTJ99@users.noreply.github.com>
2024-01-17 03:25:19 +0200

    Update and rename mapp48.js to mapp49.js

--- mapp49.js
+++ mapp49.js
@@ -292,7 +292,9 @@ map.addLayer({
   }
 
  map.on("click", "locations", (e) => {
+   console.log(e,"thiooss)
     const feature = e.features[0];
+   console.log(feature, "bbbb");
     const ID = feature.properties.arrayID;
       console.log(feature.geometry, "features");
       console.log(ID, "uuuuuuuu");
