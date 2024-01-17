82b97f03c7f806900f691f1e5a00ec5fbde94613
RTJ <46841637+RTJ99@users.noreply.github.com>
2024-01-18 00:50:00 +0200

    Update and rename mapp121.js to mapp122.js

--- mapp122.js
+++ mapp122.js
@@ -64,20 +64,22 @@ function applyFilters() {
     filterCondition.push(featuresFilter);
     console.log(filterCondition,"filter condition-------");
   }*/
-  if (selectedFeatures.length > 0 && !selectedFeatures.includes("all")) {
-    console.log("we are here");
+
+// Check if there are selected features and the array doesn't just contain 'all'
+if (selectedFeatures.length > 0 && !selectedFeatures.includes("all")) {
     const featuresFilter = ["any"];  // Initialize the filter with 'any'
 
     selectedFeatures.forEach(function (selectedFeature) {
         // For each selected feature, add a condition to the filter.
-        // This condition checks if the 'features' array in the properties of each feature includes the selected feature.
         featuresFilter.push(["in", selectedFeature, ["get", "features", ["properties"]]]);
     });
 
-    console.log(selectedFeatures, "selected features++++++++");
-    filterCondition.push(featuresFilter);  // Add the constructed filter to the filterCondition array
+    // The final filterCondition
+    let filterCondition = featuresFilter;
+
     console.log(filterCondition, "filter condition-------");
 }
+
 
   
   
