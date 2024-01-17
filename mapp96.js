d894cab2611829c8efe03a3d6e8946236c6d62e5
RTJ <46841637+RTJ99@users.noreply.github.com>
2024-01-17 19:37:44 +0200

    Update and rename mapp95.js to mapp96.js

--- mapp96.js
+++ mapp96.js
@@ -86,8 +86,29 @@ function applyFilters() {
     ]);
 
     map.setFilter("locations", filterCondition);
+
+    // Manually add the cluster layer
+    map.addLayer({
+      id: "clusters",
+      type: "circle",
+      source: "locations",
+      filter: ["has", "point_count"],
+      paint: {
+        "circle-color": [
+          "step",
+          ["get", "point_count"],
+          "#51bbd6",
+          100,
+          "#f1f075",
+          750,
+          "#f28cb1",
+        ],
+        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+      },
+    });
   }
 }
+
 
 
 
