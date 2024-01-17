5b7acfb2e08f49d88dd604b0205ed2dd5ad5c176
RTJ <46841637+RTJ99@users.noreply.github.com>
2024-01-17 19:57:25 +0200

    Update and rename mapp96.js to mapp97.js

--- mapp97.js
+++ mapp97.js
@@ -87,28 +87,13 @@ function applyFilters() {
 
     map.setFilter("locations", filterCondition);
 
-    // Manually add the cluster layer
-    map.addLayer({
-      id: "clusters",
-      type: "circle",
-      source: "locations",
-      filter: ["has", "point_count"],
-      paint: {
-        "circle-color": [
-          "step",
-          ["get", "point_count"],
-          "#51bbd6",
-          100,
-          "#f1f075",
-          750,
-          "#f28cb1",
-        ],
-        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
-      },
+    // Dynamically update the source data for the cluster layer
+    map.getSource("clusteredLocations").setData({
+      type: "FeatureCollection",
+      features: map.queryRenderedFeatures({ layers: ["locations"] }),
     });
   }
 }
-
 
 
 
