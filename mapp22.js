049c40df2ab768d740764db10013bea4c6b735eb
RTJ <46841637+RTJ99@users.noreply.github.com>
2024-01-18 09:36:30 +0200

    Update and rename mapp21.js to mapp22.js

--- mapp22.js
+++ mapp22.js
@@ -101,7 +101,7 @@ function applyFilters() {
    map.setFilter("clusters", combinedFilter);   
    console.log(combinedFilter,"###### filterCondition ####");
 
-    map.setFilter("locations", filterCondition);
+    map.setFilter("locations",combinedFilter);
 
    
    
