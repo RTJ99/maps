b9cec5cba4ecb06074d3440d01a38855705eb581
RTJ <46841637+RTJ99@users.noreply.github.com>
2024-02-07 10:34:14 +0200

    Update and rename map1.js to map.js

--- map.js
+++ map.js
@@ -49,22 +49,20 @@ $(function () {
 });
 
 function applyFilters() {
-  const selectedFeatures = $(".featureCheckbox:checked")
-    .map(function () {
-      return $(this).val();
-    })
-    .get();
+  const selectedFeatures = $(".featureCheckbox:checked").map(function () {
+    return $(this).val();
+  }).get();
 
   const selectedCountries = $("#countryDropdown").val();
 
-  let filterCondition = ["any"];
+  let combinedFilter = ["all"];
 
   if (selectedFeatures.length > 0 && !selectedFeatures.includes("all")) {
     const featuresFilter = ["any"];
     selectedFeatures.forEach(function (selectedFeature) {
       featuresFilter.push(["in", selectedFeature, ["get", "features"]]);
     });
-    filterCondition.push(featuresFilter);
+    combinedFilter.push(featuresFilter);
   }
 
   if (selectedCountries && selectedCountries.length > 0) {
@@ -72,24 +70,12 @@ function applyFilters() {
     selectedCountries.forEach(function (selectedCountry) {
       countriesFilter.push(["in", selectedCountry, ["get", "country"]]);
     });
-    filterCondition.push(countriesFilter);
+    combinedFilter.push(countriesFilter);
   }
 
-  // If "All" is selected, log and show points with vowels in the description
-  if (selectedFeatures.includes("all")) {
-    const vowelPoints = mapLocations.features.filter((feature) =>
-      /[aeiou]/i.test(feature.properties.description)
-    );
-    console.log("Points with Vowels:", vowelPoints);
-    map.setFilter("locations", [
-      "in",
-      "id",
-      ...vowelPoints.map((point) => point.properties.id),
-    ]);
-  } else {
-    // Show locations based on selected filters
-    map.setFilter("locations", filterCondition);
-  }
+  // Apply the combined filter to both clusters and locations
+  map.setFilter("locations", combinedFilter);
+  map.setFilter("clusters", combinedFilter);
 }
 
 function filterMapFeatures(selectedFeatureText) {
